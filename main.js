050c7e972513b56238f4d1db2dfef6c2b2635004
akutagawa.junki <akutagawa.junki@ditgroup.jp>
2021-01-05 18:59:32 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Electronのモジュール
+const electron = require("electron");
+
+// アプリをコントロールするモジュール
+const app = electron.app;
+
+// ウィンドウを作成するモジュール
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.on('window-all-closed', function(){
+	if (process.platform != 'darwin'){
+	  app.quit();
+  }
+});
+
+// Electronの初期化完了後に実行
+app.on('ready', function() {
+  // メイン画面の表示。ウィンドウの幅、高さを指定できる
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // ウィンドウが閉じられたらアプリも終了
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
